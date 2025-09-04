f1bebe8bae4ed2298db6511f9cf1ba13aa4ba3d6
Harvey <sa58595189@gmail.com>
2025-09-04 15:32:54 +0800

    改port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "https://personal-finance-x1hy.onrender.com",
         changeOrigin: true,
       },
     },
